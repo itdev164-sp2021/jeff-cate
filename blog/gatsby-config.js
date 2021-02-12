82b751940a6187ecf8423a4d8b64e41f1289c5a4
JeffACate <Jeff.A.Cate@gmail.com>
2021-02-11 19:13:25 -0600

    Added markdown support

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -17,10 +17,11 @@ module.exports = {
         {
             resolve: `gatsby-source-contentful`,
             options: {
-                spaceId: process.env.GATSBY_SPACE_ID,
-                accessToken: process.env.GATSBY_ACCESS_TOKEN
+                spaceId: `${process.env.SPACE_ID}`,
+                accessToken:`${process.env.ACCESS_TOKEN}`
             }
         },
+        `gatsby-transformer-remark`,
         `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-source-filesystem`,
