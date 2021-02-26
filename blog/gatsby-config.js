df4a5a0443832c91ab948721e0abe37e91c760e3
JeffACate <Jeff.A.Cate@gmail.com>
2021-02-25 19:51:44 -0600

    updated blog title for activity 5

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -5,7 +5,7 @@ require("dotenv").config({
 module.exports = {
     siteMetadata: {
         title: `Gatsby Blog`,
-        description: `ITDEV-164 Cool Gatsby Blog.`,
+        description: `Jeff Cool Gatsby Blog.`,
         author: `Jeff Cate`,
         contact:{
             name:`Jeff Cate`,
